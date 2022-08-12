668958a364cdd6e26dec6ab2946b801c7025587c
Michal Kuracina <michal.kuracina@henkel.com>
2022-08-12 12:19:26 +0200

    windows forms assembly added -> screenshot method

--- Log.to.Text.File/SessionLog.cs
+++ Log.to.Text.File/SessionLog.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
+using System.Windows.Forms;
 
 namespace Log.to.Text.File
 {
@@ -50,5 +53,36 @@ namespace Log.to.Text.File
             sw.WriteLine($"{DateTime.Now} >> EXCEPTION MESSAGE: {e.Message}");
             sw.WriteLine($"{DateTime.Now} >> STACK TRACE: {e.StackTrace}");
         }
+
+        public void Screenshot()
+        {
+            ////Create a new bitmap.
+            Bitmap bmpScreenshot = new Bitmap(
+                Screen.PrimaryScreen.Bounds.Width,
+                Screen.PrimaryScreen.Bounds.Height, 
+                PixelFormat.Format32bppArgb);
+
+            // Create a graphics object from the bitmap.
+            var gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+
+            // Take the screenshot from the upper left corner to the right bottom corner.
+            gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                        Screen.PrimaryScreen.Bounds.Y,
+                                        0,
+                                        0,
+                                        Screen.PrimaryScreen.Bounds.Size,
+                                        CopyPixelOperation.SourceCopy);
+
+            Bitmap clone = bmpScreenshot.Clone(new Rectangle(0, 0, Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height), PixelFormat.Format8bppIndexed);
+
+            string screenshotFileName = "Screenshot_" + DateTime.Now.ToString("ddMMyyyy_HHmmss") + ".png";
+
+            // Save the screenshot to the specified path that the user has chosen.
+            clone.Save(_directory + screenshotFileName, ImageFormat.Png);
+
+            // Make entry to the log
+            Write(screenshotFileName);
+        }
+
     }
 }
